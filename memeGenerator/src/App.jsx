9dc5d38766ca4657bb7e1f76a5050046e5411c78
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-03 17:50:05 +0200

    create react app using Vite
    - remove index.css
    - some styling from App.css

---
+++ memeGenerator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>JESUS</h1>
+    </>
+  )
+}
+
+export default App
